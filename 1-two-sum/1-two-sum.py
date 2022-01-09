5422465dec7ed1e121365314368ba132b93ff513
koshtiakanksha <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-09 12:52:15 +0530

    Time: 44 ms (99.86%), Space: 15.3 MB (54.45%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        HashMap = {}                              #create an empty dictionary
+        for i, n in enumerate(nums):              #iterate through each element in nums
+            diff = target - n                #target = n1+n2 -> n1= target-n2 -> n1=diff, n2=n
+            if diff in HashMap:                 #to check if the diff is present in nums(list)
+                return [HashMap[diff], i]          #to return indices of n and the diff
+            HashMap[n] = i                #update the hashmap in case the solution isn't found
+                                        #
