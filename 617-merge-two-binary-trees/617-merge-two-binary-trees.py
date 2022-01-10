45f66ae83f69b76fe76f6f4adfa32bc241716ab2
koshtiakanksha <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-10 16:04:25 +0530

    Time: 117 ms (22.18%), Space: 15.7 MB (16.01%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.py
@@ -6,18 +6,21 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root1 and not root2:
+        if not root1 and not root2:     # if root1, root2 don't exist, don't return anything
             return None
-        if root1:
+        if root1:                       # if root 1 has a value, save it to a variable
             v1 = root1.val
-        else:
+        else:                           # else save the default value as 0
             v1 = 0
         if root2:
             v2 = root2.val
         else:
             v2 = 0
-        root = TreeNode(v1 + v2)
-        root.left = self.mergeTrees(root1.left if root1 else None, root2.left if root2 else None)
-        root.right = self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)
+        root = TreeNode(v1 + v2)        # since we have to merge both trees
+        
+        root.left = self.mergeTrees(root1.left if root1 else None, root2.left if root2 else None)                                   # call the function for root.left
+        root.right = self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)                              # and for root.right
         return root
         
+        # time complexity O(m+n) 
+        # m= no of elements in tree1 and n= no of elements in tree2
