db966758ba711c716ab175c52191e3bd8b5235d2
koshtiakanksha <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-10 17:00:19 +0530

    Time: 65 ms (52.67%), Space: 15.7 MB (33.76%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -11,16 +11,16 @@ import collections                                   #very important step
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if not root:                             
+        if not root:                     # if the root doesn't exist we simply return None   
             return None
-        q = collections.deque([root])
+        q = collections.deque([root])     # initializing the queue data structure
         while q:
-            size = len(q)
+            size = len(q)                # note the size of q. very important
             for i in range(size):
-                node = q.popleft()
-                if i < size-1:
-                    node.next = q[0]
-                if node.left:
+                node = q.popleft()        #to pop the from the front of the queue
+                if i < size-1:         # to avoid establishing new pointers beyond the end
+                    node.next = q[0]    
+                if node.left:            #to add the children at the end
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
