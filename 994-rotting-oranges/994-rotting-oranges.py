c3f0c5eade35ca0510558d769d4396c502bae375
koshtiakanksha <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-12 00:19:36 +0530

    Time: 94 ms (9.11%), Space: 14.2 MB (68.10%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -3,31 +3,31 @@ class Solution:
         from collections import deque
         q = deque()
         
-        fresh = 0
+        fresh = 0              # we need to run the code until fresh turns back to 0 again
         ROWS, COLS = len(grid), len(grid[0])
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 2:
+                if grid[r][c] == 2:     # since they are rotten, add them to deque-. q
                     q.append((r, c))
-                elif grid[r][c] == 1:
+                elif grid[r][c] == 1:      # add one to the fresh if we find one fresh orange
                     fresh += 1
 
-        q.append((-1, -1))
+        q.append((-1, -1))              # to mark the round
 
         time = -1
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]       # in four directions
         while q:
-            row, col = q.popleft()
+            row, col = q.popleft()     # since we are using bfs to find the solution
             if row == -1:
-                time += 1
+                time += 1                             # after one round
                 if q:  
-                    q.append((-1, -1))
+                    q.append((-1, -1))                # to avoid endless loop
             else:
-                for d in directions:
-                    r, c = row + d[0], col + d[1]
-                    if ROWS > r >= 0 and COLS > c >= 0:
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if ROWS > r >= 0 and COLS > c >= 0:    # to check the bound
                         if grid[r][c] == 1:
-                            grid[r][c] = 2
-                            fresh -= 1
-                            q.append((r, c))
-        return time if fresh == 0 else -1
+                            grid[r][c] = 2                 # change to rotten
+                            fresh -= 1                    # decrease the no. of fresh oranges
+                            q.append((r, c))           # add to the deque
+        return time if fresh == 0 else -1            
