99329cf0f95e6f789385d54b6b7f3a6f9c0349ed
koshtiakanksha <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-16 10:39:15 +0530

    Time: 36 ms (55.51%), Space: 14.1 MB (69.42%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,10 +1,13 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        power = 31
-        res = 0
-        while n:
-            res += ((n%2)*2)**power
-            n = n >> 1
-            power = power -1
+        power = 31                              # the input is a binary string of length 32
+        res = 0                                # initially set it to zero
+        while n:                                  
+            res += ((n%2)*2)**power    # we take the mod of n, then multiply it by 2 and take                                              its power and add it to our res 
+            n = n >> 1                     # shift the integer by 1
+            power = power -1               # we decrement the power by 1 
         return res
         
+        
+# time complexity = O(1), since the integer is of fixed size
+# space complexity = O(1), since the consumption of memory is constant
