d0e5112ef6c34c2fec7226830c91197794ec38fc
koshtiakanksha <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-21 10:48:38 +0530

    Time: 472 ms (73.83%), Space: 18.9 MB (75.25%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -5,7 +5,7 @@ class Solution:
         start = 0                # we start at index at 0
         for i in range(len(gas)):         # iterate through both the list 
             curr += gas[i] - cost[i]       # we add the gass and subtract the cost from curr
-            total += gas[i] - cost[i]           # add the curr amount of gas to our total
+            total += gas[i] - cost[i]           # add the same amount of gas to our total
             if curr < 0:           # if curr is less than zero
                 start = i + 1          # we add one to our start
                 curr = 0            # and set curr back to zero
@@ -14,7 +14,7 @@ class Solution:
         else:                 # else we simply return -1, because the solution does't exists
             return -1
                 
-            
+  # on line 7, we cannot add the curr amount to out total, since we equal it to zero only if its value is less than zero          
             # time complexity = O(n)
             # space complexity = O(1)
         
