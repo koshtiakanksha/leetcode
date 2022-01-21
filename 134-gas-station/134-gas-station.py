8176b748f1839dead4374a85a0402c47accfcfbf
koshtiakanksha <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-21 10:47:12 +0530

    Time: 1076 ms (5.04%), Space: 19 MB (51.22%) - LeetHub

---
+++ 134-gas-station/134-gas-station.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total = 0       # total is the sum of all gas and costs in the list after iteration
+        curr = 0            # curr is the gas at that given point
+        start = 0                # we start at index at 0
+        for i in range(len(gas)):         # iterate through both the list 
+            curr += gas[i] - cost[i]       # we add the gass and subtract the cost from curr
+            total += gas[i] - cost[i]           # add the curr amount of gas to our total
+            if curr < 0:           # if curr is less than zero
+                start = i + 1          # we add one to our start
+                curr = 0            # and set curr back to zero
+        if total >= 0:        # we total is >= o0 i.e. the desired outcome
+            return start      # we return the value of start
+        else:                 # else we simply return -1, because the solution does't exists
+            return -1
+                
+            
+            # time complexity = O(n)
+            # space complexity = O(1)
+        
+        
