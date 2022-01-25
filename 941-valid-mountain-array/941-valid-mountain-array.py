3f81284ea9da7be8e5113fd104ebd0e03e3297e7
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-25 16:59:53 +0530

    Time: 349 ms (14.24%), Space: 15.6 MB (18.42%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -1,14 +1,18 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr)< 3:
+        if len(arr)< 3:    # if length of array is less than 3 we return False
             return False
+        # first, we'll check if one and last values are lowest
         if (arr[1] > arr[0]) and (arr[len(arr)-1]<arr[len(arr)-2]):
             i = 0
-            while arr[i]< arr[i+1]:
+            while arr[i]< arr[i+1]:    # store the value of i till its heighest, peak
                 i+=1
-            for j in range(i, len(arr)-1):
-                if arr[j]<= arr[j+1]:
+            for j in range(i, len(arr)-1):   # after peak, if the value doesn't decrease 
+                if arr[j]<= arr[j+1]:         # we return false
                     return False
-        else:
+        else:                       # if first and last aren't lowest
             return False
         return True
+    
+    # time complexity: O(n)
+    # space complexity: O(n)
