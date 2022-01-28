c51079ee430ff786c50a86480c168eaf50205d29
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-28 11:04:55 +0530

    Time: 28 ms (91.19%), Space: 13.8 MB (100.00%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,15 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums)<=3:
+        if len(nums)<=3:        # if the len is ess than 3, we return max from the list
             return (max(nums))
         
-        def max_rob(start,end):
+        def max_rob(start,end):   # we def new function, that takes the start & end index
             res1 = 0                                       #suppose it starts at index 0
-            res2 = 0            #suppose it starts at index 1
+            res2 = 0                                       #suppose it starts at index 1
         
-            for i in range(start, end):
-                temp = max(res1+nums[i], res2)
+            for i in range(start, end):          
+                temp = max(res1+nums[i], res2)# we add adj numbers ans store max in temp
                 res1 = res2
-                res2 = temp
-            return res2
-        return max(max_rob(0,len(nums)-1), max_rob(1,len(nums)))
+                res2 = temp       
+            return res2      # return the max
+        return max(max_rob(0,len(nums)-1), max_rob(1,len(nums)))   # we call the function twice, once, starting with index 0, and the other with index 1 and return the max of both
+    
+    # time complexity = O(N)
+    # space complexity = O(1)
