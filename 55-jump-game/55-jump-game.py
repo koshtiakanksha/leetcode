00c8415171a6dfc69a133341c2a1c606345e8130
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-28 11:11:03 +0530

    Time: 460 ms (94.90%), Space: 15.3 MB (72.85%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -2,11 +2,11 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goal = len(nums)-1                     # index val of last element in the list
         for i in range(len(nums)-1,-1,-1):     # we want the list in the reverse order
-            if i + nums[i]>= goal:           # if we can reach to this element before the goal
-                goal = i                   # it means that we hve to reach to the previous one
+            if i + nums[i]>= goal:     # if we can reach to this element before the goal
+                goal = i             # it means that we hve to reach to the previous one
         return goal == 0           # because after that we know we can reach the last ele
                                       # so we set our goal to the index of previous one
-                                      # at the end if goal == 0, i.e if we reach the first ele
+                      # at the end if goal == 0, i.e if we reach the first ele
                                       # we return True
     
     
