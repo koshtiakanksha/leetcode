e0757871b12cbf7176c95e5f751c09670dfa3836
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-28 23:34:43 +0530

    Time: 1358 ms (9.23%), Space: 16.5 MB (96.92%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.py
@@ -1,13 +1,15 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        if k <= 1: return 0
-        prod = 1
-        ans = left = 0
-        for right, val in enumerate(nums):
-            prod *= val
-            while prod >= k:
+        if k <= 1: return 0   # we return 0 immediately as it won't make the answer
+        prod = 1        # initially we set to 1, and not to 0, because 0 times num is 0
+        ans = left = 0     # ans is our end res and left is the pointer starting at 0
+        for right, val in enumerate(nums):# right pointer starts at 0 & val = nums[r]
+            prod *= val   # after every loop we multiply one val
+            while prod >= k:   
                 prod /= nums[left]
-                left += 1
-            ans += right - left + 1
+                left += 1     # since we use div function we increment l by 1
+            ans += right - left + 1 
         return ans
+      # Time Complexity: O(N)
+      # Space Complexity: O(1)
         
