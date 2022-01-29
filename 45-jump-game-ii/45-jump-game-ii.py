27bba45d8e7be1ca7f6f3684e1700b8c15884282
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-29 19:32:40 +0530

    Time: 161 ms (62.99%), Space: 15.1 MB (89.83%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,14 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jump = 0
-        l = 0
+        jump = 0        # initially we set jump to 0
+        l = 0            # left and right pointers are set to 0
         r = 0
-        while r < len(nums)- 1:
-            farthest = 0
-            for i in range(l, r+1):
-                farthest = max(farthest, i+nums[i])
-            l = r+1
-            r = farthest
-            jump += 1
-        return jump
+        while r < len(nums)- 1:   # till r pointer isn't equal to the index of last val
+            farthest = 0     # create a new var to store the farthest dist it can cover
+            for i in range(l, r+1):  # since the end value isn't inclusive, add 1 to r
+                farthest = max(farthest, i+nums[i])  # current index + max jump at i
+            l = r+1       # update our variables
+            r = farthest   
+            jump += 1       # increment our jump by 1
+        return jump     
+    
+    # time complexity = O(N)
+    # space complexity = O(1)
         
