dcc0f6c07eee02c745cfa8e7e2831eeb1f3c99ca
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-30 12:36:08 +0530

    Time: 76 ms (7.14%), Space: 14.1 MB (99.93%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -2,11 +2,16 @@ class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         count = 0
         total = 0
-        for i in range(2, len(nums)):
-            if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:
-                count += 1
-            else:
-                total += (count +1)*count//2
-                count = 0
-        total+=(count+1)*count//2
+        for i in range(2, len(nums)): # iterate through every number
+            if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:  # check 3 adj nums
+                count += 1                     # we increment count by 1
+            else:                            # if the chain is broken
+                total += (count +1)*count//2 # we add the formula to count
+                count = 0                     # we set the count to zero
+        total+=(count+1)*count//2    # we add the formula to count         
         return total
+    
+    # time complexity = O(N)
+    # space complexity = O(1)
+    
+    
