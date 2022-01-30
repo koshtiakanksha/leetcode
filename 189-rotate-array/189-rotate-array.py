2e37eac2fbc2ae61a543dae9c747d7444ca0161e
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-30 13:39:43 +0530

    Time: 220 ms (81.00%), Space: 25.4 MB (96.40%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -4,24 +4,21 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)     # since it may go out of bounds
+        def reverse(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l, r = l+1, r-1
         
-        # to reverse the whole array
-        l, r = 0, len(nums)-1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l+1, r-1
-            
-        # reverse only the part till the kth index
-        l, r = 0, k-1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l+1, r-1
         
-        # reverse only the part after the kth index
-        l, r = k, len(nums)-1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l+1, r-1
+        reverse(0,len(nums)-1)    # to reverse the whole array
+        reverse(0, k-1)           # reverse only the part till the kth index
+        reverse(k, len(nums)-1)   # reverse only the part after the kth index
+        
+       
+        
+       
+        
+       
             
         # time complexity = O(n)
         # space complexity = O(1)
