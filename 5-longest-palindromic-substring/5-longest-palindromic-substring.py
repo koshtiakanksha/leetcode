f4c31b9dc7883e2a56349fb0c4500bb91c9bed36
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-01-30 11:42:30 +0530

    Time: 2058 ms (31.85%), Space: 13.8 MB (99.77%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,25 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-        reslen = 0
+        res = ""                # default case
+        reslen = 0              
         for i in range(len(s)):
-            l, r = i, i
-            while l >= 0 and r< len(s) and s[l] == s[r]:
-                if (r-l+1)> reslen:
-                    res = s[l:r+1]
-                    reslen = r-l +1
-                l -= 1
-                r += 1
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l+1)> reslen:
-                    res = s[l:r+1]
-                    reslen = r-l +1
-                l -= 1
-                r += 1
+            l, r = i, i      # for odd length palindrome
+            while l >= 0 and r< len(s) and s[l] == s[r]: # 1st loop, the same letter
+                if (r-l+1)> reslen:           # since we want the longest substring
+                    res = s[l:r+1]            # we save the string
+                    reslen = r-l +1              # update the length of largest str 
+                l -= 1                         # move to left
+                r += 1                          # move to right
+            l, r = i, i + 1                # for even length
+            while l >= 0 and r < len(s) and s[l] == s[r]:  # two adj strings are equal
+                if (r-l+1)> reslen:         # since we want the longest substring
+                    res = s[l:r+1]          # we save the string
+                    reslen = r-l +1         # update the length of largest str 
+                l -= 1                      # move to left
+                r += 1                       # move to right
         return res
         
-
+# time complexity = O(N**2)
+# space complexity = O(1)
         
         
         
