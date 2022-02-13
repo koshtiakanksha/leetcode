dceb4b00617eb9e24ef3394e3ba5ead8ca5441e0
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-02-13 22:10:13 +0530

    Time: 72 ms (7.01%), Space: 14 MB (95.43%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,9 +1,10 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        output = [[]]
-        for num in nums:
-            output += [curr + [num] for curr in output]
+        output = [[]]             # create a list that contains one empty list
+        for num in nums:         # iterate through nums
+            output += [curr + [num] for curr in output]   # we add one value from the nums b                                                         list to every element in the output list
         
         return output
         
-        
+        # time complexity: O(N*2^N)
+        # space complexity: O(N*2^N)        
