efb30f2beafaaa6738c3e3715203c597b0f06d28
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-02-21 15:29:30 +0530

    Time: 164 ms (91.68%), Space: 15.5 MB (81.81%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,8 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = collections.Counter(nums)
-        return max(counts.keys(), key=counts.get)
+        count = 0
+        candidate = None
+        for i in nums:
+            if count == 0:
+                candidate = i
+            count += (1 if i == candidate else -1)
+        return candidate
     
     # time complexity = O(n)
-    # space complexity = O(n)
+    # space complexity = O(1)
         
