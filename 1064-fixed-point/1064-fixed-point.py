893594400185c4950bc8011da8b98017a4642fef
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-02-23 20:15:45 +0530

    Time: 98 ms (35.93%), Space: 15 MB (99.54%) - LeetHub

---
+++ 1064-fixed-point/1064-fixed-point.py
@@ -0,0 +1,7 @@
+class Solution:
+    def fixedPoint(self, arr: List[int]) -> int:
+        for i in range(len(arr)):
+            if i == arr[i]:
+                return i
+        return -1
+        
