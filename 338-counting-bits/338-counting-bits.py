efdf13d236d4b1e6dbfc4dad6e572799e9af3818
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-03-01 23:36:18 +0530

    Time: 75 ms (97.00%), Space: 20.8 MB (87.66%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for x in range(1, n + 1):
+            ans[x] = ans[x & (x - 1)] + 1
+        return ans
+    
+# time complexity = O(N)
+# space complexity = O(1)
