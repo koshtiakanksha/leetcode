3322f635a5dea28ed728a1c5b180291990a96039
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-03-06 22:37:22 +0530

    Time: 64 ms (99.40%), Space: 18.2 MB (23.24%) - LeetHub

---
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+
+        new_root = Node(root.val)
+        # Starting point to kick off the BFS visits.
+        queue = deque([(root, new_root)])
+
+        while queue:
+            # Get the element from the head of the queue.
+            old_node, new_node = queue.popleft()
+
+            for child_node in old_node.children:
+                new_child_node = Node(child_node.val)
+
+                # Make a copy for each child node.
+                new_node.children.append(new_child_node)
+
+                # Schedule a visit to copy the child nodes of each child node.
+                queue.append((child_node, new_child_node))
+
+        return new_root
+    
+    
