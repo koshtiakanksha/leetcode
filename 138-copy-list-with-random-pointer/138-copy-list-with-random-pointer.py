b4eabb60fa8191a7de3517578d171c966a2e545c
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-03-12 23:31:36 +0530

    Time: 53 ms (49.58%), Space: 15.4 MB (9.15%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def __init__(self):
+        # Dictionary which holds old nodes as keys and new nodes as its values.
+        self.visitedHash = {}
+
+    def copyRandomList(self, head):
+
+        if head == None:
+            return None
+
+        # If we have already processed the current node, then we simply return the cloned version of it.
+        if head in self.visitedHash:
+            return self.visitedHash[head]
+
+        # create a new node
+        # with the value same as old node.
+        node = Node(head.val, None, None)
+
+        # Save this value in the hash map. This is needed since there might be
+        # loops during traversal due to randomness of random pointers and this would help us avoid them.
+        self.visitedHash[head] = node
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+
+        return node
+        
