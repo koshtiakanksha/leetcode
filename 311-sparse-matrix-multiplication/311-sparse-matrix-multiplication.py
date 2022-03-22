7158563ab5687dfb8e1941520eb299ba253fee49
Akanksha Koshti <97248384+koshtiakanksha@users.noreply.github.com>
2022-03-22 20:03:19 +0530

    Time: 103 ms (46.81%), Space: 14.2 MB (89.10%) - LeetHub

---
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
@@ -0,0 +1,13 @@
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        ans = [[0] * len(mat2[0]) for _ in range(len(mat1))]
+        
+        for row_index, row_elements in enumerate(mat1):
+            for element_index, row_element in enumerate(row_elements):
+                if row_element:
+                    for col_index, col_element in enumerate(mat2[element_index]):
+                        ans[row_index][col_index] += row_element * col_element
+        return ans
+        
+        # time complexity = O(m * k * n)
+        # space complexity = O(1)
